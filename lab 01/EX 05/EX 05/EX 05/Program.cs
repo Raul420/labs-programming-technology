bf1a58c57bbb4a695be4229889a18300cd3c9066
Raul <smoke420weed@mail.ru>
2017-04-30 01:10:25 +0300

    sup github

---
+++ lab 01/EX 05/EX 05/EX 05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {Console.WriteLine("Please enter the triangle's perimeter");
+            string temp = Console.ReadLine();
+            double P = double.Parse(temp);
+            double p = P / 2;
+            double a = P / 3;
+            double S = Math.Sqrt(p * (p - a)*(p - a)*(p - a));
+            Console.WriteLine("SIDE\tSQUARE");
+            Console.WriteLine("{0:0.##}\t{1:0.##}", a, S);
+            Console.Read(); }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ lab02/EX03/Distance/Distance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    class Program
+    {
+        public struct Distance
+        {
+            public long ft;
+            public long inch;
+        }
+        static void Main(string[] args)
+        {
+            Distance first;
+            Distance second;
+            Distance summary;
+            Console.WriteLine("Enter first length in feet and inches");
+            first.ft = long.Parse(Console.ReadLine());
+            first.inch = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second length in feet and inches");
+            second.ft = long.Parse(Console.ReadLine());
+            second.inch = long.Parse(Console.ReadLine());
+            summary.ft = first.ft + second.ft + ((first.inch + second.inch) / 12);
+            summary.inch = ((first.inch + second.inch) % 12);
+            Console.WriteLine("{0} '- {1}''", summary.ft, summary.inch);
+        }
+    }
+}
